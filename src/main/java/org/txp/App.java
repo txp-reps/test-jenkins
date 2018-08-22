d93c306398baf7477ced6f0655da4b6238728c98
tangxiaopeng <juelu.g.001@gmail.com>
2018-08-22 17:08:56 +0800

    init project

---
+++ src/main/java/org/txp/App.java
@@ -0,0 +1,13 @@
+package org.txp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
